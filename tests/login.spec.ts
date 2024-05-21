6ec88c7646ccf9d836eb033e8c093b0d266ba9aa
SHAZZAD-BS1572 <shazzad.mia@brainstation-23.com>
2024-05-21 20:11:53 +0600

    Updated after part2

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -9,12 +9,19 @@ test('Login test demo', async () => {
 
 
   await page.goto('https://ecommerce-playground.lambdatest.io/');
+  
 
   await page.hover("//a[@role='button']//span[@class='title'][normalize-space()='My account']")
   await page.click("//span[contains(text(),'Login')]")
   await page.fill("//input[@id='input-email']", "abdulhamid@gmail.com")
   await page.fill("//input[@id='input-password']", "123456789")
   await page.click("//input[@value='Login']");
+  await page.waitForTimeout(5000);
+
+  const newContext =  await browser.newContext()
+
+  const page1 = await newContext.newPage();
+  await page1.goto("https://ecommerce-playground.lambdatest.io/index.php?route=account/account")
   await page.waitForTimeout(5000);
 
 });
